52b36498714efe8c91fdd29cd1e83761689a6168
Gavin Satur <gavin.satur@moneris.com>
2020-10-15 22:48:32 -0400

    Temporary test for autobuild

--- whois.sh
+++ whois.sh
@@ -25,11 +25,12 @@ mapfile=ip-whois; cat /dev/null > $mapfile
 # ip-api accepts batches of 100 IPs at a time for whois lookups
 #
 num_ips=`wc -l $ipfile | cut -d' ' -f1`
-last_batch=$(( num_ips % 100 ))
+#last_batch=$(( num_ips % 100 ))
+last_batch=$(( num_ips % 2 )) # Temp test
 if [ $last_batch -eq 0 ]; then
-        num_batches=$(( num_ips / 100 ))
+        num_batches=$(( num_ips / 2 )) # Temp test
 else
-        num_batches=$(( (num_ips / 100) + 1 ))
+        num_batches=$(( (num_ips / 2) + 1 )) # Temp test
 fi
 #
 # For each batch, generate the require JSON data, make the API call and process the response
@@ -38,7 +39,7 @@ bn=1
 while [ $bn -le $num_batches ]
         do
                 if [ $bn -lt $num_batches ]; then
-                        head -$(( bn * 100 )) $ipfile | tail -100 | sed 's/^/"/g;s/$/",/g;$s/,/\n]/;1 s/"/[\n"/' > ipbatch${bn}.json
+                        head -$(( bn * 2 )) $ipfile | tail -2 | sed 's/^/"/g;s/$/",/g;$s/,/\n]/;1 s/"/[\n"/' > ipbatch${bn}.json # Temp test
                         curl -X POST http://ip-api.com/batch?fields=org -d @ipbatch${bn}.json 2>/dev/null | jq '.[] |.org' > ipbatch${bn}.whois
                         cat ipbatch${bn}.json | jq '.[]' > ipbatch${bn}.ip
                         if [ `wc -l ipbatch${bn}.ip | cut -d' ' -f1` -eq `wc -l ipbatch${bn}.whois | cut -d' ' -f1` ]; then
@@ -46,8 +47,9 @@ while [ $bn -le $num_batches ]
                         else
                                 echo "ERROR : Unmatched #lines in ipbatch${bn}.whois and ipbatch${bn}.ip"
                         fi
-                        if [ $(( bn % 15 )) -eq 0 ]; then
-                                sleep 65
+                        if [ $(( bn % 3 )) -eq 0 ]; then # Temp test
+				echo "Sleeping for 5 seconds due to rate limiting requirements..."
+                                sleep 5
                         fi
                         bn=$(( bn + 1 ))
                 else
